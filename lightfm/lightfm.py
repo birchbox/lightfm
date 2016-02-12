df7b4c109479d4471c1f11e5855954cc975d1b36
Ethan Rosenthal <ethan.rosenthal@ethan-rosenthal.local>
2016-02-12 12:18:18 -0500

    Grab item bias, not user bias

--- lightfm/lightfm.py
+++ lightfm/lightfm.py
@@ -251,10 +251,10 @@ class LightFM(object):
         item_features = self._to_cython_dtype(item_features)
 
         precomputed_item_bias = np.empty(n_items, dtype=np.float32)
-        interactions = interactions.tocsr()
+        interactions = interactions.T.tocsr()
         for i in xrange(n_items):
             precomputed_item_bias[i] = np.float(interactions.getrow(i).nnz) ** self.sdpty_alpha
-        interactions = interactions.tocoo()
+        interactions = interactions.tocoo().T
 
 
         if self.item_embeddings is None:
